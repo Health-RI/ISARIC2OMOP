17f5af0ce25461a01b08c3a67351d89879aba540
Robin Navest <59924376+rnavest@users.noreply.github.com>
2024-05-23 09:04:35 +0200

    Update condition.py
    Updated cond_occ_columns
    Removed condition_yes_no_columns as all elements in cond_occ_columns are yes, no or N/A

--- isaric_to_omop/condition.py
+++ isaric_to_omop/condition.py
@@ -71,10 +71,7 @@ def populate_condition_occurence(df: pd.DataFrame, postgres: PostgresController)
                          "visit_occurrence_id", "visit_detail_id", "condition_source_value",
                          "condition_source_concept_id", "condition_status_source_value"]
 
-    cond_occ_columns = ["pregyn_rptestcd", "egestage_rptestcd", "postpart_rptestcd", "pregout_rptestcd",
-                         "dlvrdtc_rptestcd", "apsc_gestout", "apsc_brfedind", "apsc_brfedindy",
-                         "apsc_brdisdat", "apsc_dvageind", "apsc_vcageind", "chroniccard_mhyn",
-                         "chronicpul_mhyn", "asthma_mhyn", "renal_mhyn", "modliver_mhyn", "mildliv_mhyn",
+    cond_occ_columns = ["chroniccard_mhyn", "chronicpul_mhyn", "asthma_mhyn", "renal_mhyn", "modliver_mhyn", "mildliv_mhyn",
                          "chronicneu_mhyn", "malignantneo_mhyn", "chronhaemo_mhyn", "aidshiv_mhyn",
                          "obesity_mhyn", "diabetiscomp_mhyn", "diabetes_mhyn", "rheumatology_mhyr",
                          "dementia_mhyn", "malnutrition_mhyn", "smoking_mhyn", "dehydration_vsorres",
@@ -86,7 +83,6 @@ def populate_condition_occurence(df: pd.DataFrame, postgres: PostgresController)
                          "seizures_cecoccur_v2", "abdopain_ceoccur_v2", "vomit_ceoccur_v2",
                          "diarrhoea_ceoccur_v2", "conjunct_ceoccur_v2", "rash_ceoccur_v2",
                          "skinulcers_ceoccur_v2", "lymp_ceoccur_v2", "bleed_ceoccur_v2", "bleed_ceterm_v2", "bleed_cetermy_v2",
-                         "influ_mbyn", "corna_mbcat", "rsv_mbcat", "adeno_mbcat", "bact_mborres",
                          "viralpneu_ceterm", "bactpneu_ceterm", "ards_ceterm", "ardssev_ceterm",
                          "pneumothorax_ceterm", "pleuraleff_ceterm", "cryptogenic_ceterm", "bronchio_ceterm",
                          "meningitis_ceterm", "seizure_ceterm", "stroke_ceterm", "heartfailure_ceterm",
@@ -97,22 +93,12 @@ def populate_condition_occurence(df: pd.DataFrame, postgres: PostgresController)
 
     general_columns = ["dsstdat", "person_id", "hostdat", "cestdat"]
 
-    condition_yes_no_columns = ["pregyn_rptestcd", "chroniccard_mhyn", "chronicpul_mhyn", "asthma_mhyn",
-                                "renal_mhyn", "chronicneu_mhyn", "malignantneo_mhyn", "dehydration_vsorres",
-                                "cough_ceoccur_v2", "coughsput_ceoccur_v2", "coughhb_ceoccur_v2",
-                                "sorethroat_ceoccur_v2", "runnynose_ceoccur_v2", "earpain_ceoccur_v2",
-                                "wheeze_ceoccur_v2", "chestpain_ceoccur_v2", "myalgia_ceoccur_v2",
-                                "jointpain_ceoccur_v2", "shortbreath_ceoccur_v2", "headache_ceoccur_v2",
-                                "inablewalk_ceoccur_v2", "confusion_ceoccur_v2", "seizures_cecoccur_v2",
-                                "abdopain_ceoccur_v2", "vomit_ceoccur_v2", "diarrhoea_ceoccur_v2",
-                                "conjunct_ceoccur_v2", "rash_ceoccur_v2", "skinulcers_ceoccur_v2",
-                                "lymp_ceoccur_v2", "bleed_ceoccur_v2"]
     # 'flw_loss_smell' 'flw2_loss_smell' 'flw_loss_taste' 'flw2_loss_taste' "losssmell_ceoccur_v2",
     #                          "losstaste_ceoccur_v2"
     condition_df = df.copy()[general_columns + cond_occ_columns]
     condition_df = pd.melt(condition_df,
                            id_vars=general_columns,
-                           value_vars=condition_yes_no_columns)
+                           value_vars=cond_occ_columns)
 
     condition_df = condition_df.loc[condition_df["value"] == ISARICYesNo.yes]
     condition_df["condition_start_date"] = condition_df.apply(
