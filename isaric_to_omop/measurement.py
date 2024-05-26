e9114ecb46993ba46a0dba7a06d5e51cc7d8f31f
anastasianayden <anastasia.nayden@health-ri.nl>
2024-05-27 00:21:30 +0200

    filters for measurements

--- isaric_to_omop/measurement.py
+++ isaric_to_omop/measurement.py
@@ -1,17 +1,20 @@
+import logging
+
 import pandas as pd
 import re
 from core.db_connector import PostgresController
 from visit import visit_concept_type
 from utils import prepare_autoincrement_index
 
+from concept import ISARICYesNo
 
 OMOP_MEASUREMENT_HEADER = [
-        "measurement_id", "person_id", "measurement_concept_id", "measurement_date", "measurement_datetime",
-        "measurement_time", "measurement_type_concept_id", "operator_concept_id", "value_as_number",
-        "value_as_concept_id", "unit_concept_id", "range_low", "range_high", "provider_id", "visit_occurrence_id",
-        "visit_detail_id", "measurement_source_value", "measurement_source_concept_id", "unit_source_value",
-        "unit_source_concept_id", "value_source_value", "measurement_event_id", "meas_event_field_concept_id"
-    ]
+    "measurement_id", "person_id", "measurement_concept_id", "measurement_date", "measurement_datetime",
+    "measurement_time", "measurement_type_concept_id", "operator_concept_id", "value_as_number",
+    "value_as_concept_id", "unit_concept_id", "range_low", "range_high", "provider_id", "visit_occurrence_id",
+    "visit_detail_id", "measurement_source_value", "measurement_source_concept_id", "unit_source_value",
+    "unit_source_concept_id", "value_source_value", "measurement_event_id", "meas_event_field_concept_id"
+]
 
 
 class ISARIGlucoseCUnits:
@@ -23,14 +26,28 @@ class OMOPUnits:
     kg = 9529
     cm = 8582
     percent = 8554
+    # blood cell scounts
     billion_cells_per_liter = 44777588  # 10^9 per liter
     billion_L = 9444  # 10^9 per liter
     thousand_uL = 8848  # 10^3 per microliter
+    # concentrations
     umol_L = 8749  # micromole per liter
     mmol_L = 8753  # millimole per liter
     mg_dL = 8840  # milligram per decilter
+    ng_ml = 8842  # nanogram per milliliter ng/ml
+    milliequivalent_per_liter = 9557  # mEq/L
+    cells_uL = 8784  # cells per microliter
+    unit_per_liter = 8645
+    # temperature
     celsius = 586323
     fahrenheit = 9289
+    # pressure
+    kPa = 44777602
+    mmHg = 8876
+    # time
+    second = 8555
+    # other
+    ml_per_24h = 8930  # mL / 24 hours
 
 
 class ISARICTemperatureUnits:
@@ -43,9 +60,9 @@ class ISARICPressureUnits:
     mmHg = 2
 
 
-class OMOPPressureUnits:
-    kPa = 44777602
-    mmHg = 8876
+class ISARICDailyPTINRlbyn:
+    pt = 1
+    inr = 2
 
 
 class OMOPMeasConcept:
@@ -111,14 +128,48 @@ class OMOPMeasConcept:
     apvs_weight_vs = 4264825
 
 
+MEAS_CONF = {
+    "daily_fi02_lb": {"units": OMOPUnits.percent, "range_low": 0.2, "range_high": 1},
+    "daily_sao2_lb": {"units": OMOPUnits.percent},
+    "daily_pao2_lb": {"isaric_units": ISARICPressureUnits, "range_low": 0, "range_high": 500},
+    "daily_pco2_lb": {"isaric_units": ISARICPressureUnits, "range_low": 0, "range_high": 200},
+    "daily_hco3_lb": {"units": OMOPUnits.milliequivalent_per_liter},
+    "daily_baseex_lb": {"units": OMOPUnits.mmol_L},
+    "daily_gcs_vs": {"range_low": 3, "range_high": 15},
+    "systolic_vs": {"units": OMOPUnits.mmHg},
+    "diastolic_vs": {"units": OMOPUnits.mmHg},
+    "daily_meanart_vs": {"units": OMOPUnits.mmHg, "range_low": 10, "range_high": 200},
+    "daily_urine_lb": {"units": OMOPUnits.ml_per_24h, "range_low": 0, "range_high": 15000},
+    "daily_lymp_lb": {"units": OMOPUnits.cells_uL, "range_low": 10, "range_high": 200},
+    "daily_neutro_lb": {"units": OMOPUnits.cells_uL, "range_low": 10, "range_high": 200},
+    "daily_haematocrit_lb": {"units": OMOPUnits.percent},
+    "daily_pt_lb": {"units": OMOPUnits.second, "range_low": 10, "range_high": 100},
+    "daily_inr_lb": {"range_low": 10, "range_high": 100},
+    "daily_alt_lb": {"units": OMOPUnits.unit_per_liter, "range_low": 0, "range_high": 8000},
+    "daily_bil_lb": {"range_low": 0, "range_high": 700},
+    "daily_ast_lb": {"units": OMOPUnits.unit_per_liter, "range_low": 0, "range_high": 8000},
+}
+
+
 def populate_measurements(df: pd.DataFrame, postgres: PostgresController):
-    general_columns = ["daily_dsstdat", "person_id", "hostdat", "cestdat", "dsstdat"]
+    general_columns = ["daily_dsstdat", "person_id", "hostdat", "cestdat", "dsstdat", "daily_lbdat"]
     # measurement column name pattern
     pattern = r"([a-z_]*_(vs|lb))(yn|orres(u)?)$"
     # select measurement columns dynamically
     measurement_columns = [x for x in df.columns if re.match(pattern, x)]
+    if "daily_lbperf" in df.columns:
+        measurement_columns.append("daily_lbperf")
 
     measurements_df = df.copy()[general_columns + measurement_columns]
+    # filter pt/inr before stacking columns
+    # daily_pt_lborres - show the field ONLY if: [daily_pt_inr_lbyn] = '1'; daily_inr_lborres - show the field ONLY if:
+    # [daily_pt_inr_lbyn] = '2'
+    if "daily_pt_inr_lbyn" in df.columns:
+        measurements_df.loc[measurements_df["daily_pt_inr_lbyn"].apply(
+            lambda x: int(x) == ISARICDailyPTINRlbyn.pt if pd.notnull(x) else False), "daily_inr_lbyn"] = None
+        measurements_df.loc[measurements_df["daily_pt_inr_lbyn"].apply(
+            lambda x: int(x) == ISARICDailyPTINRlbyn.inr if pd.notnull(x) else False), "daily_pt_lbyn"] = None
+        measurements_df.drop(columns=["daily_pt_inr_lbyn"], inplace=True)
 
     # combine availability, result and units to a dictionary and melt the frame
     core_meas_names = []
@@ -127,7 +178,12 @@ def populate_measurements(df: pd.DataFrame, postgres: PostgresController):
             core_name = re.match(pattern, column).group(1)
             core_meas_names.append(core_name)
             measurements_df[core_name] = measurements_df.apply(
-                lambda x: {"value": x[column], "avail": x.get(f"{core_name}yn"), "unit": x.get(f"{column}u")}, axis="columns")
+                lambda x: {"value": x[column],
+                           "avail": x.get(f"{core_name}yn"),
+                           "unit": x.get(f"{column}u"),
+                           "daily_lbperf": x.get("daily_lbperf")
+                           },
+                axis="columns")
 
     measurements_df = pd.melt(measurements_df,
                               id_vars=general_columns,
@@ -136,6 +192,18 @@ def populate_measurements(df: pd.DataFrame, postgres: PostgresController):
     values_df = pd.json_normalize(measurements_df['value'])
     measurements_df.drop(columns="value", inplace=True)
     measurements_df = pd.merge(left=measurements_df, right=values_df, left_index=True, right_index=True)
+
+    # filter out laboratory data if daily_lbperf != 1 and measyrement xxx_lbyn != 1
+    if pd.notnull(measurements_df["daily_lbperf"]).all() and pd.notnull(measurements_df["avail"]).all():
+        logging.warning("Either 'daily_lbperf' is not in the data or empty and/or all xxx_vsyn and xxx_lbyn columns"
+                        "are empty, measurements data will be ignored")
+    measurements_df.loc[
+        (measurements_df["variable"].str.endswith("_lb")) &
+        ~(measurements_df["daily_lbperf"].apply(
+            lambda x: int(x) == ISARICYesNo.yes if pd.notnull(x) else False)), "avail"] = None
+    measurements_df = measurements_df.loc[
+        measurements_df["avail"].apply(lambda x: int(x) == ISARICYesNo.yes if pd.notnull(x) else False)]
+
     measurements_df = measurements_df.loc[pd.notnull(measurements_df["value"])]
     # todo filter on avail
 
@@ -151,16 +219,24 @@ def populate_measurements(df: pd.DataFrame, postgres: PostgresController):
     no_concept = measurements_df.loc[pd.isnull(measurements_df["measurement_concept_id"]), "variable"].values.tolist()
     if no_concept:
         print(no_concept)
-    # todo dates
-    measurements_df.rename(columns={"daily_dsstdat": "measurement_date",
-                                    "value": "value_source_value",
-                                    "unit": "concept_id"}, inplace=True)
+    # Set dates
+    measurements_df.loc[
+        measurements_df["variable"].str.endswith("_lb"), "measurement_date"] = measurements_df["daily_lbdat"]
+    measurements_df.loc[
+        pd.isnull(measurements_df["measurement_date"]), "measurement_date"] = measurements_df["daily_dsstdat"]
+    measurements_df.loc[
+        pd.isnull(measurements_df["measurement_date"]), "measurement_date"] = measurements_df["hostdat"]
+    measurements_df.loc[
+        pd.isnull(measurements_df["measurement_date"]), "measurement_date"] = measurements_df["dsstdat"]
+    measurements_df.rename(columns={
+        "value": "value_source_value",
+        "unit": "unit_concept_id"}, inplace=True)
     measurements_df["measurement_type_concept_id"] = visit_concept_type.concept_id
 
     measurements_df = measurements_df.loc[pd.notnull(measurements_df["measurement_concept_id"])]
     measurements_df = prepare_autoincrement_index(measurements_df,
-                                                 "measurement",
-                                                 "measurement_id",
-                                                 postgres)
+                                                  "measurement",
+                                                  "measurement_id",
+                                                  postgres)
     measurement_df = measurements_df.reindex(columns=OMOP_MEASUREMENT_HEADER)
     postgres.df_to_postgres(table="measurement", df=measurement_df)
